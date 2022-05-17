b30bc700e106a8b313eb6af142f365819c3330b4
Praveendhar <www.praveendhar@gmail.com>
2022-05-17 18:56:52 +0530

    Chart of accounts groups

--- Proxlight_Designer_Export/Proxlight_Designer_Export/window.py
+++ Proxlight_Designer_Export/Proxlight_Designer_Export/window.py
@@ -17,23 +17,73 @@ from tkinter import ttk
 #     global Canvas3
 #     Canvas3 = tk.Canvas(background="#e6ffff", insertbackground="black", relief="ridge",selectbackground="blue", selectforeground="white")
 #     Canvas3.place(relx=0.850, rely=0.100, relheight=0.8, relwidth=0.150)
+
+def chartofaccountsgroups():
+    global Canvas1
+    Canvas1 = tk.Canvas( background="#ffffff", relief="ridge").place(relx=0, rely=0.07, relheight=0.900, relwidth=.850)
+
+    global Canvas3
+    Canvas2 = tk.Canvas( background="#ffffff", relief="ridge").place(relx=0, rely=0.07, relheight=0.900, relwidth=.850)
+
+    Label1 = Label(Canvas2,text='List of masters', background="white",foreground="black",font="-family {Segoe UI} -size 15 -weight bold ").place(relx=0, rely=0.07,relwidth=.100)
+    b1 = Button(Canvas2,text = "Groups/Divisions",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.12)
+    b2 = Button(Canvas2,text = "Capital Account",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.15)
+    b3 = Button(Canvas2,text = "Reserves & Surplus",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.18)
+    b4 = Button(Canvas2,text = "Current Assets",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.21)
+    b5 = Button(Canvas2,text = "Bank Accounts",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.24)
+    b6 = Button(Canvas2,text = "Cash in Hand",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.27)
+    b7 = Button(Canvas2,text = "Deposits (Asset)",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.30)
+    b8 = Button(Canvas2,text = "Loans & Advances (Asset)",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.33)
+    b9 = Button(Canvas2,text = "Stock-in-Hand",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.36)
+    b10 = Button(Canvas2,text = "Sundry Debtors",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.39)
+    b11 = Button(Canvas2,text = "Current Liabilities",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.42)
+    b12 = Button(Canvas2,text = "Duties & Taxes",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.45)
+    b13 = Button(Canvas2,text = "Provisions",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.48)
+    b14 = Button(Canvas2,text = "Sundry Creditors",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.51)
+    b15 = Button(Canvas2,text = "Direct Expenses",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.54)
+    b16 = Button(Canvas2,text = "Direct Incomes",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.57)
+    b17 = Button(Canvas2,text = "Fixed Assets",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.60)
+    b18 = Button(Canvas2,text = "Inderect Expenses",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.63)
+    b19 = Button(Canvas2,text = "Inderect Incomes",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.66)
+    b20 = Button(Canvas2,text = "Investments",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.69)
+    b21 = Button(Canvas2,text = "Loans (Liability)",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.72)
+    b22 = Button(Canvas2,text = "Bank OD A/c",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.75)
+    b23 = Button(Canvas2,text = "Secured Loans",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.78)
+    b24 = Button(Canvas2,text = "Unsecured Loans",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.02, rely=0.81)
+    b21 = Button(Canvas2,text = "Misc Expenses (ASSET)",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.84)
+    b21 = Button(Canvas2,text = "Purchase Accounts",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.87)
+    b21 = Button(Canvas2,text = "Sales Accounts",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.90)
+    b21 = Button(Canvas2,text = "Suspense A/c",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0, rely=0.93)
+   
+
+
 def chartofaccounts():
     global Canvas1
     Canvas1 = tk.Canvas( background="#ffffff", relief="ridge").place(relx=0, rely=0.07, relheight=0.900, relwidth=.850)
-    Canvas1
 
     global Canvas3
     Canvas3 = tk.Canvas(background="#e6ffff", insertbackground="black", relief="ridge",selectbackground="blue", selectforeground="white").place(relx=0.850, rely=0.07, relheight=0.9, relwidth=0.150)
 
     global canvas4
-    Canvas4 = tk.Canvas( background="#ffffff", relief="ridge").place(relx=0.550, rely=0.4, relheight=0.200, relwidth=.150)
+    Canvas4 = tk.Canvas( background="lightblue", relief="sunken").place(relx=0.35, rely=0.3, relheight=0.500, relwidth=.200)
 
-    Label1 = Label(Canvas3,text='List of masters', background="#3385ff",foreground="#00254a",font="-family {Segoe UI} -size 10 -weight bold ").place(relx=0.550, rely=0.400,relwidth=.150)
+    Label1 = Label(Canvas3,text='List of masters', background="#3385ff",foreground="#00254a",font="-family {Segoe UI} -size 10 -weight bold ").place(relx=0.35, rely=0.29,relwidth=.200)
     # Label2 = Label(Canvas3,text='Accounting masters', background="#3385ff",foreground="#00254a",font="-family {Segoe UI} -size 10 -weight bold ").place(relx=0.550, rely=0.450,relwidth=.150)
     # Label3 = Label(Canvas3,text='Inventory masters', background="#3385ff",foreground="#00254a",font="-family {Segoe UI} -size 10 -weight bold ").place(relx=0.550, rely=0.402,relwidth=.150)
-    b1 = Button(Canvas3,text = "Groups",activeforeground = "black", activebackground = "#ffbe23",fg='#3385ff',bg='#ffffff',borderwidth=0,font=('Arial 10'),command=chartofaccounts).place(relx=0.551, rely=0.450,relwidth=.148)
-    b2 = Button(Canvas3,text = "Ledgers",activeforeground = "black", activebackground = "#ffbe23",fg='#3385ff',bg='#ffffff',borderwidth=0,font=('Arial 10')).place(relx=0.551, rely=0.480,relwidth=.148)
-    b3 = Button(Canvas3,text = "Quit",activeforeground = "black", activebackground = "#ffbe23",fg='#3385ff',bg='#ffffff',borderwidth=0,font=('Arial 10')).place(relx=0.551, rely=0.520,relwidth=.148)
+    Label2 = Label(Canvas3,text='Accounting masters', background="Lightblue",foreground="#00254a",font="-family {Segoe UI} -size 10 -weight bold ").place(relx=0.35, rely=0.33,relwidth=.200)
+    b1 = Button(Canvas3,text = "Groups",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10'),command=chartofaccountsgroups).place(relx=0.35, rely=0.36,relwidth=.200)
+    b2 = Button(Canvas3,text = "Ledgers",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.39,relwidth=.200)
+    b3 = Button(Canvas3,text = "Voucher Types",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.42,relwidth=.200)
+    b4 = Button(Canvas3,text = "Currencies",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.45,relwidth=.200)
+    b5 = Button(Canvas3,text = "Budgets",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.48,relwidth=.200)
+    b6 = Button(Canvas3,text = "Scenarios",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.51,relwidth=.200)
+    Label3 = Label(Canvas3,text='Inventory masters', background="Lightblue",foreground="#00254a",font="-family {Segoe UI} -size 10 -weight bold ").place(relx=0.35, rely=0.54,relwidth=.200)
+    b7 = Button(Canvas3,text = "Stock Groups",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.57,relwidth=.200)
+    b8 = Button(Canvas3,text = "Stock Items",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.60,relwidth=.200)
+    b9 = Button(Canvas3,text = "Stock Catogories",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.63,relwidth=.200)
+    b10 = Button(Canvas3,text = "Units",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.66,relwidth=.200)
+    b11 = Button(Canvas3,text = "Godowns",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=0.69,relwidth=.200)
+    b = Button(Canvas3,text = "Quit",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='lightblue',borderwidth=0,font=('Arial 10')).place(relx=0.35, rely=72,relwidth=.200)
 
 global screen
 screen=Tk()
@@ -78,5 +128,5 @@ b12 = Button(screen,text = "Quit",activeforeground = "black", activebackground =
 # Chart of accounts section
 
 
-
+a=chartofaccountsgroups()
 screen.mainloop()
